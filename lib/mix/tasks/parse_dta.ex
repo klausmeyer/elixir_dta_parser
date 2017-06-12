1aed3be7a7731e991ba39404f3af50fa5917e79d
Klaus Meyer <spam@klaus-meyer.net>
2017-06-12 09:55:42 +0200

    rework mix task

--- lib/mix/tasks/parse_dta.ex
+++ lib/mix/tasks/parse_dta.ex
@@ -2,7 +2,14 @@ defmodule Mix.Tasks.ParseDta do
   use Mix.Task
 
   @shortdoc "Parse DTA file and output it's content"
-  def run(_) do
-    DtaParser.parse("data/sample.dta")
+  def run(args) do
+    case args do
+      [] -> parse()
+      _  -> parse(hd(args))
+    end
+  end
+
+  defp parse(file \\ "data/sample.dta") do
+    DtaParser.parse(file)
   end
 end
